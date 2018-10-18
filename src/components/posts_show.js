93c81916dae01dbe9a33a86253a6fadef5960f85
andylushman <andylushman@gmail.com>
2018-10-18 10:54:34 -0600

    added delete functionality

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,9 +1,52 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPost, deletePost } from "../actions";
+import { Link } from "react-router-dom";
 
 class PostShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
+  }
+
   render() {
-    return <div>Post Show</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
-export default PostShow;
+//first argument is applicaiton state, second argument is the props that id being injectected into the component, if added.
+function mapStateToProps({ posts }, ownProps) {
+  // to just give one specific post instead of the list from application state.
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchPost, deletePost }
+)(PostShow);
